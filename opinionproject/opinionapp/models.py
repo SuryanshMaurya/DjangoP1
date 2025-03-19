10deeea782b1d67bc9d2dad2733e96f0b1e04c79
Pritam mann <preetam1202204@gmail.com>
2025-03-19 15:52:32 +0530

    created the database and do the first opinion
    created the database and do the first opinion

--- opinionproject/opinionapp/models.py
+++ opinionproject/opinionapp/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+class Opinion(models.Model):
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    message = models.TextField()
+    title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class reaction(models.Model):
+    likes = models.IntegerField(default=0)
+    comments = models.IntegerField(default=0)
+    opinion = models.ForeignKey(Opinion, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.opinion.title
+    
+class user(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+# Opinion
+#   ├── Reaction (Comment 1)
+#   │     ├── Reaction (Reply to Comment 1)
+#   │     │     └── Reaction (Reply to Reply to Comment 1)
+#   │     └── Reaction (Another Reply to Comment 1)
+#   └── Reaction (Comment 2)
+#         └── Reaction (Reply to Comment 2)
--- opinionproject/opinionapp/views.py
+++ opinionproject/opinionapp/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from opinionapp.models import Opinion
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def opinion(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        message = request.POST['message']
+        image = request.FILES.get('image')  # Corrected syntax
+        opinion = Opinion(title=title, message=message, image=image)
+        opinion.save()
+    return render(request, 'post.html')
